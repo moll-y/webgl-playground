060e8948f0cee046065065b8798f642d34addf68
Cristian Camilo Serna Betancur <mol.tired@gmail.com>
2025-07-30 07:15:21 -0500

    build: add glsl plugin

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,13 +1,14 @@
-const esbuild = require("esbuild");
-const glob = require("glob");
-const path = require("path");
-const fs = require("fs");
+import { build } from "esbuild";
+import { glsl } from "esbuild-plugin-glsl";
+import { glob } from "glob";
+import path from "path";
+import fs from "fs";
 
 glob.sync("assets/js/*/main.js").forEach((entry) => {
   const id = path.basename(path.dirname(entry));
   const dir = `static/js/${id}`;
   fs.mkdirSync(dir, { recursive: true });
-  esbuild.build({
+  build({
     entryPoints: [entry],
     bundle: true,
     minify: true,
@@ -17,6 +18,17 @@ glob.sync("assets/js/*/main.js").forEach((entry) => {
     outfile: `${dir}/bundle.js`,
     alias: {
       "@": path.resolve("assets/js"),
+      "@glsl": path.resolve("assets/glsl"),
+    },
+    plugins: [
+      glsl({
+        minify: true,
+      }),
+    ],
+    loader: {
+      ".glsl": "text",
+      ".vert": "text",
+      ".frag": "text",
     },
   });
 });
